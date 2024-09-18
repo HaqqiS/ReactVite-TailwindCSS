41cb7b8c3937225ab54158a230e372a252d2256b
“haqqi <haqqi30sukmara@gmail.com>
2024-09-18 20:50:08 +0800

    v1.16 useEffect dan lifecycle component

--- src/Pages/product.jsx
+++ src/Pages/product.jsx
@@ -1,6 +1,6 @@
 import Button from "../components/Elements/Button";
 import CardProduct from "../components/Fragments/CardProduct";
-import { React, useState } from "react";
+import { React, useEffect, useState } from "react";
 
 const products = [
     {
@@ -29,12 +29,41 @@ const products = [
 const email = localStorage.getItem("email");
 
 const ProductsPage = () => {
-    const [cart, setCart] = useState([
-        {
-            id: 1,
-            qty: 1,
-        },
-    ]);
+    // state untuk menyimpan data cart
+    // data cart berupa array of object yang berisi id produk, qty produk, dan harga produk
+    const [cart, setCart] = useState([]);
+    // state untuk menyimpan total harga dari cart
+    // state ini digunakan untuk menampilkan total harga di halaman product
+    const [totalPrice, setTotalPrice] = useState(0);
+    /**
+     * Saat component pertama kali di render, maka akan menjalankan useEffect ini.
+     * Didalam useEffect ini, kita akan mengambil data cart yang tersimpan di localStorage
+     * dan mengisikan ke state cart dengan menggunakan setCart.
+     * Jika data cart tidak ada di localStorage, maka state cart akan diisi dengan array kosong.
+     */
+    useEffect(() => {
+        setCart(JSON.parse(localStorage.getItem("cart")) || []);
+    }, []);
+    // Saat state cart berubah, maka akan menjalankan useEffect ini.
+    // Didalam useEffect ini, kita akan menghitung total harga dari cart
+    // dan mengisikan ke state totalPrice dengan menggunakan setTotalPrice.
+    useEffect(() => {
+        // Jika cart tidak kosong, maka hitung total harga nya
+        if (cart.length > 0) {
+            // Buat variabel sum untuk menyimpan hasil perhitungan total harga
+            const sum = cart.reduce((acc, item) => {
+                // Cari produk yang id nya sama dengan item.id
+                const product = products.find((product) => product.id === item.id);
+                // Tambahkan harga produk yang dikalikan dengan qty produk
+                // ke dalam variabel acc
+                return acc + product.price * item.qty;
+            }, 0);
+            // Set state totalPrice dengan hasil perhitungan total harga
+            setTotalPrice(sum);
+            // Simpan state cart ke localStorage
+            localStorage.setItem("cart", JSON.stringify(cart));
+        }
+    }, [cart]);
 
     const handleLogout = () => {
         localStorage.removeItem("email");
@@ -42,14 +71,18 @@ const ProductsPage = () => {
         window.location.href = "/login";
     };
 
+    /**
+     * Fungsi ini digunakan untuk menambahkan produk ke dalam cart.
+     * Jika produk sudah ada dalam cart, maka qty produk akan ditambahkan 1.
+     * Jika produk belum ada dalam cart, maka produk akan ditambahkan ke dalam cart dengan qty 1.
+     * @param {number} id - id produk yang akan ditambahkan ke dalam cart
+     */
     const handleAddToCart = (id) => {
         if (cart.find((item) => item.id === id)) {
-            setCart(
-                cart.map((item) =>
-                    item.id === id ? { ...item, qty: item.qty + 1 } : item
-                )
-            );
+            // Jika produk sudah ada dalam cart, maka qty produk akan ditambahkan 1
+            setCart(cart.map((item) => (item.id === id ? { ...item, qty: item.qty + 1 } : item)));
         } else {
+            // Jika produk belum ada dalam cart, maka produk akan ditambahkan ke dalam cart dengan qty 1
             setCart([...cart, { id, qty: 1 }]);
         }
     };
@@ -72,14 +105,8 @@ const ProductsPage = () => {
                     {products.map((product) => (
                         <CardProduct key={product.id}>
                             <CardProduct.Header image={product.image} />
-                            <CardProduct.Body name={product.name}>
-                                {product.description}
-                            </CardProduct.Body>
-                            <CardProduct.Footer
-                                price={product.price}
-                                id={product.id}
-                                handleAddToCart={handleAddToCart}
-                            />
+                            <CardProduct.Body name={product.name}>{product.description}</CardProduct.Body>
+                            <CardProduct.Footer price={product.price} id={product.id} handleAddToCart={handleAddToCart} />
                         </CardProduct>
                     ))}
                 </div>
@@ -96,26 +123,19 @@ const ProductsPage = () => {
                         </thead>
                         <tbody>
                             {cart.map((item) => {
-                                const product = products.find(
-                                    (product) => product.id === item.id
-                                );
+                                const product = products.find((product) => product.id === item.id);
                                 return (
                                     <tr key={item.id}>
                                         <td>{product.name}</td>
                                         <td>
-                                            {product.price.toLocaleString(
-                                                "id-ID",
-                                                {
-                                                    style: "currency",
-                                                    currency: "IDR",
-                                                }
-                                            )}
+                                            {product.price.toLocaleString("id-ID", {
+                                                style: "currency",
+                                                currency: "IDR",
+                                            })}
                                         </td>
                                         <td>{item.qty}</td>
                                         <td>
-                                            {(
-                                                item.qty * product.price
-                                            ).toLocaleString("id-ID", {
+                                            {(item.qty * product.price).toLocaleString("id-ID", {
                                                 style: "currency",
                                                 currency: "IDR",
                                             })}
@@ -123,6 +143,15 @@ const ProductsPage = () => {
                                     </tr>
                                 );
                             })}
+                            <tr className="font-bold">
+                                <td colSpan={3}>Price</td>
+                                <td>
+                                    {totalPrice.toLocaleString("id-ID", {
+                                        style: "currency",
+                                        currency: "IDR",
+                                    })}
+                                </td>
+                            </tr>
                         </tbody>
                     </table>
                 </div>
